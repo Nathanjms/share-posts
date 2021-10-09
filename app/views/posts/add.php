681f2c3f5bb05a4874a838139d5abc967d94c68e
Nathan James <nathanjms@outlook.com>
2021-10-09 14:00:18 +0100

    'Add Post' form

---
+++ app/views/posts/add.php
@@ -0,0 +1,18 @@
+<a href="<?= URLROUTE; ?>/posts" class="btn btn-light w-100"><i class="fas fa-long-arrow-alt-left"></i></a>
+<div class="card card-body bg-light mt-3">
+    <h2>Add Post</h2>
+    <p>Create a post with this form</p>
+    <form action="<?= URLROUTE; ?>/posts/add" method="post">
+        <div class="form-group">
+            <label for="title">Title: <sup>*</sup></label>
+            <input type="title" name="title" id="title" value="<?= $data['title']; ?>" class="form-control form-control-lg <?= (!empty($data['title_error'])) ? 'is-invalid' : ''; ?>">
+            <span class="invalid-feedback"><?= $data['title_error']; ?></span>
+        </div>
+        <div class="form-group">
+            <label for="body">Body: <sup>*</sup></label>
+            <textarea name="body" id="body" class="form-control form-control-lg <?= (!empty($data['body_error'])) ? 'is-invalid' : ''; ?>"><?= $data['title']; ?></textarea>
+            <span class="invalid-feedback"><?= $data['body_error']; ?></span>
+        </div>
+        <input type="submit" value="Login" class="btn btn-success mt-2 w-100">
+    </form>
+</div>
